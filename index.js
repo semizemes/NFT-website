4748d33496ecaff8da38ad90c0345d1135ad53a8
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-17 14:19:51 +0500

    feat(footer): auto-update footer year to current year via index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// javascript
+const year = document.getElementById("year");
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+console.log(currentYear);
+console.log(year);
+year.textContent = currentYear;
